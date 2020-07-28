3d43267a0927ebc6e1bcbdf6b9860bd47ff675b7
Danielle Hillman <hillm108@mail.chapman.edu>
2020-07-27 19:11:52 -0700

    added score and timer

--- extra.js
+++ extra.js
@@ -124,6 +124,35 @@ function nextQuestion(number) {
   
   
   
+
+
+
+
+function nextQ () {
+
+    document.getElementById('number1').textContent = myQuestions[currentIndex].question
+    let allanswers = myQuestions[currentIndex].answers
+    document.getElementbyId('allanswers').textContent = myQuestions[currentIndex].answers
+  
+    document.getElementById('results').innerHTML = ''
+    document.getElementById('count').textContent = `${count} Seconds` 
+    // document.getElementById('number1').textContent = `
+    // ${myQuestions[z].question}`
+    startButton.classList.add('hide')
+    let answers = myQuestions[currentIndex].answers
+  
+  
+  
+    for (let i=0; i < answers.length; i++) {
+      let answerElem = document.createElement('button')
+      answerElem.className = 'btn answer'
+      answerELem.dataset.answer = answers[i]
+      answerElem.textContent = answers[i]
+      document.getElementbyID('answer').append(answerELem)
+  
+    }
+  }
+  
   
   
   // if (event.target.classList.contains('correct') && myQuestions[0].correctAnswer === 'b') {
@@ -277,6 +306,385 @@ function nextQuestion(number) {
   
   
   
+const startButton = document.getElementById('start')
+const nextButton = document.getElementById('next')
+const correct = document.getElementById('right')
+const wrong = document.getElementById('wrong')
+
+
+const a = document.getElementById('A')
+const b = document.getElementById('B')
+const c = document.getElementById('C')
+const d = document.getElementById('D')
+
+
+function startGame (z) {
+  document.getElementById('results').innerHTML = ''
+  document.getElementById('count').textContent = `
+  ${count} Seconds` 
+  document.getElementById('number1').innerHTML = `
+  ${myQuestions[z].question}`
+  document.getElementById('A').innerHTML = `
+  ${myQuestions[z].a} `
+  document.getElementById('A').classList.remove('hide')
+  document.getElementById('B').innerHTML = `
+  ${myQuestions[z].b} `
+  document.getElementById('B').classList.remove('hide')
+  document.getElementById('C').innerHTML = `
+  ${myQuestions[z].c} `
+  document.getElementById('C').classList.remove('hide')
+  document.getElementById('D').innerHTML = `
+  ${myQuestions[z].d} `
+  document.getElementById('D').classList.remove('hide')
+  nextButton.classList.remove('hide')
+}
+
+
+
+
+  
+document.getElementById('start').addEventListener('click', event => {
+  event.preventDefault()
+  startButton.classList.add('hide')
+
+
+  myInterval = setInterval(() => {
+    
+    // displays time left on screen
+    count--
+    document.getElementById('count').textContent = `
+    ${count} Seconds
+    `
+    // moves count down by 1
+    if (count <= 0) {
+
+      console.log('works')
+      clearInterval(myInterval)
+      document.getElementById('count').innerHTML = ``
+      document.getElementById('question1').innerHTML = `
+      finished
+      `
+      // document.getElementById('number1').innerHTML = `
+      // Great job! Your score is: ${score}
+      // `
+    }
+  }, 1000)
+
+  startGame(0)
+}
+
+)
+
+// for (i = 1; i< 3;i++) { 
+//   document.getElementById('next').addEventListener('click', event => {
+//     document.getElementById('next').classList.add('next2')
+//  startGame(2)
+// }
+  
+// )
+
+
+
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('test') && myQuestions[0].correctAnswer === 'b') {
+    document.getElementById('score1').innerHTML = `1`
+    nextButton.classList.remove('hide')
+    document.getElementById('results').innerHTML = 'right'
+    // correct.classList.remove('hide') 
+    startGame(1)
+  } else {
+    document.getElementById('results').innerHTML = 'wrong'
+  // wrong.classList.remove('hide')
+  count = count - 10 }
+  
+  })
+  document.getElementById('next').addEventListener('click', event => {
+    document.getElementById('next').classList.add('next2')
+ startGame(2)
+})
+document.getElementById('next2').addEventListener('click', event => {
+  document.getElementById('next').classList.add('next2')
+startGame(0)
+}
+)
+
+
+
+
+
+
+
+// document.addEventListener('click', event => {
+// if (event.target.classList.contains('test') && myQuestions[0].correctAnswer === 'b') {
+//   document.getElementById('score1').innerHTML = `1`
+//   nextButton.classList.remove('hide')
+//   correct.classList.remove('hide')
+//   nextQuestion(1)
+// } else {
+// wrong.classList.remove('hide')
+// count = count - 10 }
+
+// }) 
+
+
+
+
+
+//  document.getElementById('next').addEventListener('click', event => { 
+    
+//     event.preventDefault()
+//     // for (i = 1; i < 4; i++ ){ 
+//     startButton.classList.add('hide')
+//     correct.classList.add('hide')
+//     wrong.classList.add('hide')
+    
+//     document.getElementById('number1').innerHTML = `
+//     ${myQuestions[1].question}`
+//     document.getElementById('A').innerHTML = `
+//     ${myQuestions[1].a} `
+//     document.getElementById('B').innerHTML = `
+//     ${myQuestions[1].b} `
+//     document.getElementById('C').innerHTML = `
+//     ${myQuestions[1].c} `
+//     document.getElementById('D').innerHTML = `
+//     ${myQuestions[1].d} `
+//     // nextButton.classList.add('hide')
+//     nextButton.classList.add('next2')
+// })
+
+
+// function lastQuestion(){
+//   document.getElementById('number1').innerHTML = `
+//   ${myQuestions[2].question}`
+//   document.getElementById('A').innerHTML = `
+//   ${myQuestions[2].a} `
+//   document.getElementById('B').innerHTML = `
+//   ${myQuestions[2].b} `
+//   document.getElementById('C').innerHTML = `
+//   ${myQuestions[2].c} `
+//   document.getElementById('D').innerHTML = `
+//   ${myQuestions[2].d} `
+  
+// }
+
+
+
+// lastQuestion ()
+
+// function tester(){
+//   document.getElementById('number1').innerHTML = `
+//   ${myQuestions[3].question}`
+//   document.getElementById('A').innerHTML = `
+//   ${myQuestions[3].a} `
+//   document.getElementById('B').innerHTML = `
+//   ${myQuestions[3].b} `
+//   document.getElementById('C').innerHTML = `
+//   ${myQuestions[3].c} `
+//   document.getElementById('D').innerHTML = `
+//   ${myQuestions[3].d} `
+  
+// }
+
+  // document.getElementById('next2').addEventListener('click', event => {
+//     event.preventDefault()
+//     // for (i = 1; i < 4; i++ ){ 
+//     startButton.classList.add('hide')
+//     correct.classList.add('hide')
+//     wrong.classList.add('hide')
+    
+//     document.getElementById('number1').innerHTML = `
+//     ${myQuestions[2].question}`
+//     document.getElementById('A').innerHTML = `
+//     ${myQuestions[2].a} `
+//     document.getElementById('B').innerHTML = `
+//     ${myQuestions[2].b} `
+//     document.getElementById('C').innerHTML = `
+//     ${myQuestions[2].c} `
+//     document.getElementById('D').innerHTML = `
+//     ${myQuestions[2].d} `
+//     // nextButton.classList.add('hide')
+//     nextButton.classList.add('next3')
+//   }
+//   )
+
+
+//  document.getElementById('next3').addEventListener('click', event => {
+//   event.preventDefault()
+//   // for (i = 1; i < 4; i++ ){ 
+//   startButton.classList.add('hide')
+//   correct.classList.add('hide')
+//   wrong.classList.add('hide')
+  
+//   document.getElementById('number1').innerHTML = `
+//   ${myQuestions[3].question}`
+//   document.getElementById('A').innerHTML = `
+//   ${myQuestions[3].a} `
+//   document.getElementById('B').innerHTML = `
+//   ${myQuestions[3].b} `
+//   document.getElementById('C').innerHTML = `
+//   ${myQuestions[3].c} `
+//   document.getElementById('D').innerHTML = `
+//   ${myQuestions[3].d} `
+//   // nextButton.classList.add('hide')
+//   // nextButton.classList.replace('next2') 
+// })
+
+
+
+// for (i = 1; i< 3;i++) { 
+//   document.getElementById('next').addEventListener('click', event => {
+//     document.getElementById('next').classList.add('next2')
+//  startGame(2)
+// }
+  
+// )
+
+
+
+// document.addEventListener('click', event => {
+//   if (event.target.classList.contains('test') && myQuestions[0].correctAnswer === 'b') {
+//     document.getElementById('score1').innerHTML = `1`
+//     nextButton.classList.remove('hide')
+//     document.getElementById('results').innerHTML = 'right'
+//     // correct.classList.remove('hide') 
+//     startGame(1)
+//   } else {
+//     document.getElementById('results').innerHTML = 'wrong'
+//   // wrong.classList.remove('hide')
+//   count = count - 10 }
+  
+//   })
+//   document.getElementById('next').addEventListener('click', event => {
+//     document.getElementById('next').classList.add('next2')
+//  startGame(2)
+// })
+// document.getElementById('next2').addEventListener('click', event => {
+//   document.getElementById('next').classList.add('next2')
+// startGame(0)
+// }
+// )
+
+
+
+
+
+
+
+// document.addEventListener('click', event => {
+// if (event.target.classList.contains('test') && myQuestions[0].correctAnswer === 'b') {
+//   document.getElementById('score1').innerHTML = `1`
+//   nextButton.classList.remove('hide')
+//   correct.classList.remove('hide')
+//   nextQuestion(1)
+// } else {
+// wrong.classList.remove('hide')
+// count = count - 10 }
+
+// }) 
+
+
+
+
+
+//  document.getElementById('next').addEventListener('click', event => { 
+    
+//     event.preventDefault()
+//     // for (i = 1; i < 4; i++ ){ 
+//     startButton.classList.add('hide')
+//     correct.classList.add('hide')
+//     wrong.classList.add('hide')
+    
+//     document.getElementById('number1').innerHTML = `
+//     ${myQuestions[1].question}`
+//     document.getElementById('A').innerHTML = `
+//     ${myQuestions[1].a} `
+//     document.getElementById('B').innerHTML = `
+//     ${myQuestions[1].b} `
+//     document.getElementById('C').innerHTML = `
+//     ${myQuestions[1].c} `
+//     document.getElementById('D').innerHTML = `
+//     ${myQuestions[1].d} `
+//     // nextButton.classList.add('hide')
+//     nextButton.classList.add('next2')
+// })
+
+
+// function lastQuestion(){
+//   document.getElementById('number1').innerHTML = `
+//   ${myQuestions[2].question}`
+//   document.getElementById('A').innerHTML = `
+//   ${myQuestions[2].a} `
+//   document.getElementById('B').innerHTML = `
+//   ${myQuestions[2].b} `
+//   document.getElementById('C').innerHTML = `
+//   ${myQuestions[2].c} `
+//   document.getElementById('D').innerHTML = `
+//   ${myQuestions[2].d} `
+  
+// }
+
+
+
+// lastQuestion ()
+
+// function tester(){
+//   document.getElementById('number1').innerHTML = `
+//   ${myQuestions[3].question}`
+//   document.getElementById('A').innerHTML = `
+//   ${myQuestions[3].a} `
+//   document.getElementById('B').innerHTML = `
+//   ${myQuestions[3].b} `
+//   document.getElementById('C').innerHTML = `
+//   ${myQuestions[3].c} `
+//   document.getElementById('D').innerHTML = `
+//   ${myQuestions[3].d} `
+  
+// }
+
+  // document.getElementById('next2').addEventListener('click', event => {
+//     event.preventDefault()
+//     // for (i = 1; i < 4; i++ ){ 
+//     startButton.classList.add('hide')
+//     correct.classList.add('hide')
+//     wrong.classList.add('hide')
+    
+//     document.getElementById('number1').innerHTML = `
+//     ${myQuestions[2].question}`
+//     document.getElementById('A').innerHTML = `
+//     ${myQuestions[2].a} `
+//     document.getElementById('B').innerHTML = `
+//     ${myQuestions[2].b} `
+//     document.getElementById('C').innerHTML = `
+//     ${myQuestions[2].c} `
+//     document.getElementById('D').innerHTML = `
+//     ${myQuestions[2].d} `
+//     // nextButton.classList.add('hide')
+//     nextButton.classList.add('next3')
+//   }
+//   )
+
+
+//  document.getElementById('next3').addEventListener('click', event => {
+//   event.preventDefault()
+//   // for (i = 1; i < 4; i++ ){ 
+//   startButton.classList.add('hide')
+//   correct.classList.add('hide')
+//   wrong.classList.add('hide')
+  
+//   document.getElementById('number1').innerHTML = `
+//   ${myQuestions[3].question}`
+//   document.getElementById('A').innerHTML = `
+//   ${myQuestions[3].a} `
+//   document.getElementById('B').innerHTML = `
+//   ${myQuestions[3].b} `
+//   document.getElementById('C').innerHTML = `
+//   ${myQuestions[3].c} `
+//   document.getElementById('D').innerHTML = `
+//   ${myQuestions[3].d} `
+//   // nextButton.classList.add('hide')
+//   // nextButton.classList.replace('next2') 
+// })
+
   
   
   
--- script.js
+++ script.js
@@ -1,46 +1,50 @@
 const myQuestions = [
   {
       question: "Pretend this is a really hard question",
-          a: "Yes" = true,
-          b: "No" = true,
-          c: "Maybe" = false,
-          d: "yo" = false,
-          correctAnswer: "b"
+      answers: [
+          "Yes",
+          "No",
+          "Maybe",
+          "Yo", ],
+          correctAnswer: "No"
   },
   {
       question: "Pretend this is a really easy question",
-          a: "Yes",
-          b: "No",
-          c: "Maybe",
-          d: "yo",
-      correctAnswer: "c"
+      answers: [
+          "Yes",
+          "No",
+          "Maybe",
+          "Yo", ],
+      correctAnswer: "Maybe"
   },
   {
       question: "Pretend you enjoy taking this quiz!",
-          a: "Yes",
-          b: "No",
-          c: "Maybe",
-          d: "I dont know",
-          correctAnswer: "d"
+      answers: [
+          "Yes",
+          "No",
+          "Maybe",
+          "I dont know",],
+          correctAnswer: "Yes"
      
   },
   {
     question: "Last Question",
-        a: "Yes",
-        b: "No",
-        c: "Maybe",
-        d: "I dont know",
-        correctAnswer: "d"
+    answers: [
+        "Yes",
+        "No",
+        "Maybe",
+        "I dont know",],
+        correctAnswer: "I dont Know"
    
 }
 ];
 
-console.log(myQuestions[0])
 
 count = 90
 let myInterval
 let question = 0
-score = 0
+let score = 0
+let currentIndex = 0
 
 const startButton = document.getElementById('start')
 const nextButton = document.getElementById('next')
@@ -48,208 +52,109 @@ const correct = document.getElementById('right')
 const wrong = document.getElementById('wrong')
 
 
-const a = document.getElementById('A')
-const b = document.getElementById('B')
-const c = document.getElementById('C')
-const d = document.getElementById('D')
+function nextQ () {
 
+  document.getElementById('number1').textContent = myQuestions[currentIndex].question
+  let answers = myQuestions[currentIndex].answers 
+  
+  document.getElementById('count').textContent = `${count} Seconds` 
 
-function startGame (z) {
-  document.getElementById('count').textContent = `
-  ${count} Seconds` 
-  document.getElementById('number1').innerHTML = `
-  ${myQuestions[z].question}`
-  document.getElementById('A').innerHTML = `
-  ${myQuestions[z].a} `
-  document.getElementById('A').classList.remove('hide')
-  document.getElementById('B').innerHTML = `
-  ${myQuestions[z].b} `
-  document.getElementById('B').classList.remove('hide')
-  document.getElementById('C').innerHTML = `
-  ${myQuestions[z].c} `
-  document.getElementById('C').classList.remove('hide')
-  document.getElementById('D').innerHTML = `
-  ${myQuestions[z].d} `
-  document.getElementById('D').classList.remove('hide')
-  nextButton.classList.remove('hide')
+  document.getElementById('allAnswers').innerHTML = ''
+
+  startButton.classList.add('hide')
+
+  for (let i=0; i < answers.length; i++) {
+    let answerElem = document.createElement('button')
+    answerElem.className = 'btn answer'
+    answerElem.dataset.answer = answers[i]
+    answerElem.textContent = answers[i] 
+    document.getElementById('allAnswers').append(answerElem)
+    nextButton.classList.add('hide') 
+  }
+  
 }
 
 
 
-
-  
 document.getElementById('start').addEventListener('click', event => {
   event.preventDefault()
   startButton.classList.add('hide')
-
+  nextQ() 
 
   myInterval = setInterval(() => {
-    
-    // displays time left on screen
     count--
     document.getElementById('count').textContent = `
     ${count} Seconds
     `
-    // moves count down by 1
     if (count <= 0) {
-
-      console.log('works')
       clearInterval(myInterval)
-      document.getElementById('count').innerHTML = ``
-      document.getElementById('question1').innerHTML = `
-      finished
-      `
-      // document.getElementById('number1').innerHTML = `
-      // Great job! Your score is: ${score}
-      // `
     }
   }, 1000)
-
-  startGame(0)
 }
-
 )
 
-for (i = 1; i< 3;i++) {
-  document.getElementById('next').addEventListener('click', event => {
- startGame(i)
+document.getElementById('next').addEventListener('click', event => {
+  event.preventDefault()
+  startButton.classList.add('hide')
+  nextQ()
 }
-)}
+)
+
+
+document.addEventListener('click', event => { 
+if (event.target.classList.contains('answer')) {
+  if (myQuestions[currentIndex].correctAnswer === event.target.dataset.answer) {
+      console.log('Answer is Correct')
+      let resultsElem = document.createElement('div')
+      resultsElem.className = 'alert'
+      resultsElem.textContent = 'Correct!'
+      document.getElementById('allAnswers').append(resultsElem)
+      score ++
+  
+} else {
+  console.log('Answer is Incorrect')
+  let resultsElem = document.createElement('div')
+      resultsElem.className = 'alert'
+      resultsElem.textContent = 'Wrong!'
+      document.getElementById('allAnswers').append(resultsElem)
+}
+currentIndex ++
+console.log(currentIndex)
+nextButton.classList.remove('hide')
 
 
 
 
-  if (event.target.classList.contains('test') && myQuestions[0].correctAnswer === 'b') {
-    document.getElementById('score1').innerHTML = `1`
-    nextButton.classList.remove('hide')
-    correct.classList.remove('hide')
-    startGame(1)
-  } else {
-  wrong.classList.remove('hide')
-  count = count - 10 }
+// setTimeout(()=> {
+
+  // if (currentIndex < myQuestions.length) {
+  //   document.getElementById('allAnswers').append('')
+  //   resultsElem.textContent = '' 
+  //   nextQ()
+  // } else {
   
 
-  
-
-
-
-
-
-
-
-// document.addEventListener('click', event => {
-// if (event.target.classList.contains('test') && myQuestions[0].correctAnswer === 'b') {
-//   document.getElementById('score1').innerHTML = `1`
-//   nextButton.classList.remove('hide')
-//   correct.classList.remove('hide')
-//   nextQuestion(1)
-// } else {
-// wrong.classList.remove('hide')
-// count = count - 10 }
-
-// }) 
-
-
-
-
-
-//  document.getElementById('next').addEventListener('click', event => { 
-    
-//     event.preventDefault()
-//     // for (i = 1; i < 4; i++ ){ 
-//     startButton.classList.add('hide')
-//     correct.classList.add('hide')
-//     wrong.classList.add('hide')
-    
-//     document.getElementById('number1').innerHTML = `
-//     ${myQuestions[1].question}`
-//     document.getElementById('A').innerHTML = `
-//     ${myQuestions[1].a} `
-//     document.getElementById('B').innerHTML = `
-//     ${myQuestions[1].b} `
-//     document.getElementById('C').innerHTML = `
-//     ${myQuestions[1].c} `
-//     document.getElementById('D').innerHTML = `
-//     ${myQuestions[1].d} `
-//     // nextButton.classList.add('hide')
-//     nextButton.classList.add('next2')
-// })
-
-
-// function lastQuestion(){
-//   document.getElementById('number1').innerHTML = `
-//   ${myQuestions[2].question}`
-//   document.getElementById('A').innerHTML = `
-//   ${myQuestions[2].a} `
-//   document.getElementById('B').innerHTML = `
-//   ${myQuestions[2].b} `
-//   document.getElementById('C').innerHTML = `
-//   ${myQuestions[2].c} `
-//   document.getElementById('D').innerHTML = `
-//   ${myQuestions[2].d} `
-  
+  //   alert('game is over')
+  // }
 // }
-
-
-
-// lastQuestion ()
-
-// function tester(){
-//   document.getElementById('number1').innerHTML = `
-//   ${myQuestions[3].question}`
-//   document.getElementById('A').innerHTML = `
-//   ${myQuestions[3].a} `
-//   document.getElementById('B').innerHTML = `
-//   ${myQuestions[3].b} `
-//   document.getElementById('C').innerHTML = `
-//   ${myQuestions[3].c} `
-//   document.getElementById('D').innerHTML = `
-//   ${myQuestions[3].d} `
-  
+// , 1000)
 // }
+}})
+ 
+// quinton's code 
+const getScore = submission => {
 
-  // document.getElementById('next2').addEventListener('click', event => {
-//     event.preventDefault()
-//     // for (i = 1; i < 4; i++ ){ 
-//     startButton.classList.add('hide')
-//     correct.classList.add('hide')
-//     wrong.classList.add('hide')
-    
-//     document.getElementById('number1').innerHTML = `
-//     ${myQuestions[2].question}`
-//     document.getElementById('A').innerHTML = `
-//     ${myQuestions[2].a} `
-//     document.getElementById('B').innerHTML = `
-//     ${myQuestions[2].b} `
-//     document.getElementById('C').innerHTML = `
-//     ${myQuestions[2].c} `
-//     document.getElementById('D').innerHTML = `
-//     ${myQuestions[2].d} `
-//     // nextButton.classList.add('hide')
-//     nextButton.classList.add('next3')
-//   }
-//   )
+console.log(submission)
+
+let leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || []
+leaderboard.push(submission)
+
+localStorage.setItem('leaderboard', JSON.stringify(leaderboard))
+
+leaderboard.sort((a,b) => {
+return b.score - a.score
+
+})
 
 
-//  document.getElementById('next3').addEventListener('click', event => {
-//   event.preventDefault()
-//   // for (i = 1; i < 4; i++ ){ 
-//   startButton.classList.add('hide')
-//   correct.classList.add('hide')
-//   wrong.classList.add('hide')
-  
-//   document.getElementById('number1').innerHTML = `
-//   ${myQuestions[3].question}`
-//   document.getElementById('A').innerHTML = `
-//   ${myQuestions[3].a} `
-//   document.getElementById('B').innerHTML = `
-//   ${myQuestions[3].b} `
-//   document.getElementById('C').innerHTML = `
-//   ${myQuestions[3].c} `
-//   document.getElementById('D').innerHTML = `
-//   ${myQuestions[3].d} `
-//   // nextButton.classList.add('hide')
-//   // nextButton.classList.replace('next2') 
-// })
-
-
+}
